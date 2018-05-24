50401af292fd491acdb8029bf0a7c009000562f1
JP Lynch <JPLynch35@gmail.com>
2018-05-24 10:10:08 -0600

    Add book class

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,13 @@
 class Book
-
+  attr_reader :author_first_name,
+              :author_last_name,
+              :title,
+              :publication_date
+  def initialize(b_info = {author_first_name: f_name, author_last_name: l_name, title: b_title, publication_date: p_date})
+      @author_first_name = b_info[:author_first_name]
+      @author_last_name = b_info[:author_last_name]
+      @title = b_info[:title]
+      @publication_date = b_info[:publication_date]
+  end
 
 end
