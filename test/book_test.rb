8988c7a3dee20432c05c9c1af76db38ceca08723
JP Lynch <JPLynch35@gmail.com>
2018-05-24 10:10:27 -0600

    Add book tests

--- test/book_test.rb
+++ test/book_test.rb
@@ -1,6 +1,35 @@
 require './test/test_helper'
+require './lib/book'
 
 class BookTest < Minitest::Test
+  def test_it_exists
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+    assert_instance_of Book, book
+  end
+
+  def test_author_first_name
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+    actual = book.author_first_name
+    assert_equal "Harper", actual
+  end
+
+  def test_author_last_name
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+    actual = book.author_last_name
+    assert_equal "Lee", actual
+  end
+
+  def test_title
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+    actual = book.title
+    assert_equal "To Kill a Mockingbird", actual
+  end
+
+  def test_publication_date
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+    actual = book.publication_date
+    assert_equal "July 11, 1960", actual
+  end
 
 
 end
